dccb4fd52490e2fadc1f129284ca029ffe48c520
0010200303 <jonas.schoenbrodt@gmail.com>
2026-04-16 14:14:53 +0200

    tust19

--- include/initializers/line.hpp
+++ include/initializers/line.hpp
@@ -16,7 +16,7 @@ namespace kocs::initializer {
 
     KOKKOS_INLINE_FUNCTION
     void operator() (const unsigned int i) const {
-      // positions_view(i) = Vector(float(i));
+      positions_view(i) = Vector(float(i));
       // positions_view(i).x() = float(i);
       positions_view(i).data[0] = float(i);
 
