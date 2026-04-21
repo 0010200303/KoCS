c68ff2ccf87f9c00a62368aaddc5695ab5b86021
0010200303 <jonas.schoenbrodt@gmail.com>
2026-04-21 10:54:13 +0200

    test 1

--- include/forces/detail.hpp
+++ include/forces/detail.hpp
@@ -35,6 +35,66 @@ namespace kocs::detail {
 
   constexpr ForceTagger<GenericForceTag> generic_force{};
   constexpr ForceTagger<PairwiseForceTag> pairwise_force{};
+
+
+
+  template<typename T>
+  struct AccumulatorSlot {
+    T value{};
+
+    KOKKOS_INLINE_FUNCTION
+    AccumulatorSlot() = default;
+
+    KOKKOS_INLINE_FUNCTION
+    AccumulatorSlot(const T& v) : value(v) { }
+
+    KOKKOS_INLINE_FUNCTION
+    T& get() {
+      return value;
+    }
+
+    KOKKOS_INLINE_FUNCTION
+    const T& get() const {
+      return value;
+    }
+
+    KOKKOS_INLINE_FUNCTION
+    AccumulatorSlot& operator+=(const AccumulatorSlot& other) {
+      value += other.value;
+      return *this;
+    }
+  };
+
+  template<typename... Slots>
+  struct AccumulatorPack : Slots... {
+    AccumulatorPack() = default;
+    AccumulatorPack(Slots... types) : Slots(types)... { }
+
+    KOKKOS_INLINE_FUNCTION
+    AccumulatorPack& operator+=(const AccumulatorPack& other) {
+      ((static_cast<Slots&>(*this) += static_cast<const Slots&>(other)), ...);
+      return *this;
+    }
+
+    template<typename Func>
+    KOKKOS_INLINE_FUNCTION
+    decltype(auto) apply(Func&& func) {
+      return func(static_cast<Slots&>(*this).get()...);
+    }
+
+    template<typename Func>
+    KOKKOS_INLINE_FUNCTION
+    decltype(auto) apply(Func&& func) const {
+      return func(static_cast<const Slots&>(*this).get()...);
+    }
+  };
+
+  template<typename... Views>
+  static auto make_accumulator_pack(const ViewPack<Views...>& pack) {
+    return AccumulatorPack<AccumulatorSlot<typename Views::value_type>...>(
+      AccumulatorSlot<typename Views::value_type>{}...
+    );
+  }
 } // namespace kocs::detail
 
 #endif // KOCS_FORCES_DETAIL_HPP
--- include/pair_finders/all_pairs.hpp
+++ include/pair_finders/all_pairs.hpp
@@ -4,6 +4,7 @@
 #include <Kokkos_Core.hpp>
 
 #include "../integrators/detail.hpp"
+#include "../forces/detail.hpp"
 
 namespace kocs::pair_finders {
   template<typename Force, typename... Views>
@@ -11,25 +12,39 @@ namespace kocs::pair_finders {
     Kokkos::parallel_for(
       "naive_all_pairs_apply_force",
       Kokkos::TeamPolicy<>(agent_count, Kokkos::AUTO()),
-      KOKKOS_LAMBDA(const Kokkos::TeamPolicy<>::member_type& team) {
-        const int i = team.league_rank();
+      KOKKOS_LAMBDA(const Kokkos::TeamPolicy<>::member_type& team_member) {
+        const int i = team_member.league_rank();
+
+        auto total = detail::make_accumulator_pack(view_pack);
+
+        Kokkos::parallel_reduce(
+          Kokkos::TeamThreadRange(team_member, agent_count),
+          [&](const int j, auto& local) {
+            local.apply([&](auto&... values) {
+              force(i, j, values...);
+            });
+          },
+          total
+        );
+
+
 
         // Kokkos::parallel_for(
-          // Kokkos::TeamThreadRange(team, agent_count),
-          // [&](const int j) {
-            // if (i == j)
-              // return;
+        //   Kokkos::TeamThreadRange(team, agent_count),
+        //   [&](const int j) {
+        //     if (i == j)
+        //       return;
 
-            // force(i, j, static_cast<const Views&>(view_pack)(i)...);
-          // }
+        //     force(i, j, static_cast<const Views&>(view_pack)(i)...);
+        //   }
         // );
 
-        for (int j = 0; j < static_cast<int>(agent_count); ++j) {
-          if (i == j)
-            continue;
+        // for (int j = 0; j < static_cast<int>(agent_count); ++j) {
+        //   if (i == j)
+        //     continue;
 
-          force(i, j, static_cast<const Views&>(view_pack)(i)...);
-        }
+        //   force(i, j, static_cast<const Views&>(view_pack)(i)...);
+        // }
       }
     );
   }
