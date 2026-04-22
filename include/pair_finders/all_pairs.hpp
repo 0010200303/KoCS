a94a1fcd44afa5e6803ed536d8571264334fdbf6
0010200303 <jonas.schoenbrodt@gmail.com>
2026-04-22 07:15:01 +0200

    test 31

--- include/pair_finders/all_pairs.hpp
+++ include/pair_finders/all_pairs.hpp
@@ -59,12 +59,13 @@ namespace kocs::pair_finders {
             },
             total
           );
+          
+          Kokkos::printf("%d %d\n", agent_count, positions.extent(0));
 
           // Kokkos::single(
-            // Kokkos::PerTeam(team_member),
-            // [&]() {
-              // Kokkos::printf("%d %d\n", agent_count, positions.extent(0));
-            // }
+          //   Kokkos::PerTeam(team_member),
+          //   [&]() {
+          //   }
           // );
 
           // Kokkos::single(
