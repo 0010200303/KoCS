77a247a9ea473d98c21f3cfb137d7e23a464a204
0010200303 <jonas.schoenbrodt@gmail.com>
2026-04-23 11:27:02 +0200

    testing auto kernel fuser

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <Kokkos_Core.hpp>
 
+#include <tuple>
+#include <type_traits>
+#include <utility>
+
 #include "include/kocs.hpp"
 
 using namespace kocs;
@@ -11,21 +15,23 @@ struct SimulationConfig : public DefaultSimulationConfig {
 };
 EXTRACT_TYPES_FROM_SIMULATION_CONFIG(SimulationConfig)
 
-template<typename... Forces>
+template<typename Tag, typename... Forces>
 struct Merger;
 
-template<>
-struct Merger<> {
+template<typename Tag>
+struct Merger<Tag> {
   Merger() = default;
+
+  using tag = Tag;
 
   template<typename... Args>
   KOKKOS_INLINE_FUNCTION
   void operator()(Args&&...) const { }
 };
 
-template<typename FirstForce, typename... RestForces>
-struct Merger<FirstForce, RestForces...> : Merger<RestForces...> {
-  using base_type = Merger<RestForces...>;
+template<typename Tag, typename FirstForce, typename... RestForces>
+struct Merger<Tag, FirstForce, RestForces...> : Merger<Tag, RestForces...> {
+  using base_type = Merger<Tag, RestForces...>;
 
   FirstForce force;
 
@@ -45,8 +51,40 @@ struct Merger<FirstForce, RestForces...> : Merger<RestForces...> {
   }
 };
 
+template<typename Tag, typename... Forces>
+Merger(Tag, Forces...) -> Merger<Tag, Forces...>;
+
+template<typename Tag, typename Force>
+auto collect_tagged_force(Force&& force) {
+  using decayed = std::decay_t<Force>;
+
+  if constexpr (std::is_same_v<typename decayed::tag, Tag>) {
+    return std::tuple<decayed>(std::forward<Force>(force));
+  } else {
+    return std::tuple<>{};
+  }
+}
+
+template<typename Tag, typename... Forces>
+auto merge_for_tag(Forces&&... forces) {
+  auto selected = std::tuple_cat(
+    collect_tagged_force<Tag>(std::forward<Forces>(forces))...
+  );
+
+  return std::apply([](auto&&... tagged_forces) {
+    return Merger<Tag, std::decay_t<decltype(tagged_forces)>...>{
+      std::forward<decltype(tagged_forces)>(tagged_forces)...
+    };
+  }, selected);
+}
+
 template<typename... Forces>
-Merger(Forces...) -> Merger<Forces...>;
+auto merge(Forces&&... forces) {
+  return std::make_tuple(
+    merge_for_tag<detail::GenericForceTag>(std::forward<Forces>(forces)...),
+    merge_for_tag<detail::PairwiseForceTag>(std::forward<Forces>(forces)...)
+  );
+}
 
 int main() {
   Simulation<SimulationConfig> sim(128);
@@ -59,8 +97,12 @@ int main() {
   Writer<SimulationConfig> writer("./output/tust");
   writer.write(0, sim);
 
-  auto generic_force = GENERIC_FORCE(unsigned int i, Random& rng, Vector& force, float& mass) {
-    mass += rng.drand(-100.0, 100.0);
+  auto generic_force_mass = GENERIC_FORCE(unsigned int i, Vector& force, float& mass) {
+    mass += 1.0;
+  };
+
+  auto generic_force_pos = GENERIC_FORCE(unsigned int i, Vector& force, float& mass) {
+    force += Vector(100.0, 0.0, 0.0);
   };
 
   const float stiffness = 0.1f;
@@ -97,29 +139,19 @@ int main() {
     force.z() += displacement.z() * (stiffness - distance) / distance;
   };
 
-  auto pairwise_force_merged = PAIRWISE_FORCE(
-    unsigned int i,
-    unsigned int j,
-    const Vector& displacement,
-    const Scalar& distance,
-    Vector& force,
-    float& mass
-  ) {
-    pairwise_force_x(i, j, displacement, distance, force, mass);
-    pairwise_force_y(i, j, displacement, distance, force, mass);
-    pairwise_force_z(i, j, displacement, distance, force, mass);
-  };
+  auto merged_forces = merge(
+    pairwise_force_x,
+    pairwise_force_y,
+    pairwise_force_z,
+    generic_force_pos,
+    generic_force_mass
+  );
 
   for (int i = 1; i <= 10; ++i) {
-    sim.take_step_rng(0.001, generic_force);
-    // sim.take_step(0.001, pairwise_force_x);
-    // sim.take_step(0.001, pairwise_force_y);
-    // sim.take_step(0.001, pairwise_force_z);
-
-    // sim.take_step(0.001, pairwise_force_x, pairwise_force_y, pairwise_force_z);
-
-    auto merged = Merger{pairwise_force_x, pairwise_force_y, pairwise_force_z};
-    sim.take_step(0.001, merged | detail::pairwise_force);
+    std::apply([&](auto&&... args) {
+        sim.take_step(0.001, args...);
+      }, merged_forces
+    );
 
     writer.write(i, sim);
   }
