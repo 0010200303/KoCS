4b8f1a09c1a706ac2e7b3e29209138c46625c9c7
0010200303 <jonas.schoenbrodt@gmail.com>
2026-04-24 07:34:53 +0200

    testing kernel fusing performance

--- include/forces/kernel_fuser.hpp
+++ include/forces/kernel_fuser.hpp
@@ -48,14 +48,22 @@ namespace kocs::detail {
   template<typename Tag, typename... Forces>
   KernelFuser(Tag, Forces...) -> KernelFuser<Tag, Forces...>;
 
-  template<typename Tag, typename Force>
+  // template<typename Tag, typename Force>
+  // auto collect_tagged_force(Force&& force) {
+  //   if constexpr (std::is_same_v<typename std::decay_t<Force>::tag, Tag>) {
+  //     using pure_force_t = std::decay_t<decltype(std::forward<Force>(force).force)>;
+  //     return std::tuple<pure_force_t>(std::forward<Force>(force).force);
+  //   } else {
+  //     return std::tuple<>{};
+  //   }
+  // }
+
+  template<typename Tag, typename... Forces>
   auto collect_tagged_force(Force&& force) {
-    if constexpr (std::is_same_v<typename std::decay_t<Force>::tag, Tag>) {
-      using pure_force_t = std::decay_t<decltype(std::forward<Force>(force).force)>;
-      return std::tuple<pure_force_t>(std::forward<Force>(force).force);
-    } else {
+    if constexpr (std::is_same_v<typename std::decay_t<Force>::tag, Tag>)
+      return std::tuple<std::decay_t<Force>>(std::forward<Force>(force));
+    else
       return std::tuple<>{};
-    }
   }
 
   template<typename Tag, typename... Forces>
