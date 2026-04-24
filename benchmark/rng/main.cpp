0f0033067bd2e9c1ee78e64f67c6953fc5d07730
0010200303 <jonas.schoenbrodt@gmail.com>
2026-04-24 11:55:34 +0200

    testing

--- benchmark/rng/main.cpp
+++ benchmark/rng/main.cpp
@@ -46,8 +46,8 @@ double benchmark_kernel(
   Kokkos::Timer timer;
 
   for (int i = 0; i < steps; ++i) {
-    sim.take_step(dt, kernel);
-    // sim.take_step(dt, kernel_rng);
+    // sim.take_step(dt, kernel);
+    sim.take_step(dt, kernel_rng);
   }
 
   Kokkos::fence();
@@ -82,8 +82,8 @@ double benchmark_rng_kernel(
   Kokkos::Timer timer;
 
   for (int i = 0; i < steps; ++i) {
-    sim.take_step_single(dt, kernel);
-    // sim.take_step_rng(dt, kernel_rng);
+    // sim.take_step_single(dt, kernel);
+    sim.take_step_rng(dt, kernel_rng);
   }
 
   Kokkos::fence();
