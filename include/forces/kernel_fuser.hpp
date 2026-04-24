199315a4d2f4b77b62d2834224bb7b61a20480cc
0010200303 <jonas.schoenbrodt@gmail.com>
2026-04-24 07:26:03 +0200

    testing kernel fusing performance

--- include/forces/kernel_fuser.hpp
+++ include/forces/kernel_fuser.hpp
@@ -34,7 +34,8 @@ namespace kocs::detail {
     KernelFuser() = default;
 
     KOKKOS_INLINE_FUNCTION
-    KernelFuser(FirstForce first, RestForces... rest) : base_type(rest...), force(first) { }
+    KernelFuser(FirstForce first, RestForces... rest)
+      : base_type(std::move(rest)...), force(std::move(first)) { }
 
     template<typename... Args>
     KOKKOS_INLINE_FUNCTION
@@ -49,21 +50,24 @@ namespace kocs::detail {
 
   template<typename Tag, typename Force>
   auto collect_tagged_force(Force&& force) {
-    if constexpr (std::is_same_v<typename std::decay_t<Force>::tag, Tag>)
-      return std::tuple<std::decay_t<Force>>(std::forward<Force>(force));
-    else
-      return std::tuple<>{};
+    // if constexpr (std::is_same_v<typename std::decay_t<Force>::tag, Tag>) {
+      // using pure_force_t = std::decay_t<decltype(std::forward<Force>(force).force)>;
+      // return std::tuple<pure_force_t>(std::forward<Force>(force).force);
+    // } else {
+      // return std::tuple<>{};
+    // }
 
-    // return std::tuple<std::decay_t<Force>>(std::forward<Force>(force));
+    using pure_force_t = std::decay_t<decltype(std::forward<Force>(force).force)>;
+    return std::tuple<pure_force_t>(std::forward<Force>(force).force);
   }
 
   template<typename Tag, typename... Forces>
   auto fuse_forces_for_tag(Forces&&... forces) {
     auto selected = std::tuple_cat(collect_tagged_force<Tag>(std::forward<Forces>(forces))...);
 
-    return std::apply([](auto&&... tagged_kernels) {
-      return KernelFuser<Tag, std::decay_t<decltype(tagged_kernels)>...> {
-        std::forward<decltype(tagged_kernels)>(tagged_kernels)...
+    return std::apply([](auto&&... kernels) {
+      return KernelFuser<Tag, std::decay_t<decltype(kernels)>...> {
+        std::forward<decltype(kernels)>(kernels)...
       };
     }, selected);
   }
@@ -72,8 +76,8 @@ namespace kocs::detail {
   template<typename... Forces>
   auto fuse_forces(Forces&&... forces) {
     return std::make_tuple(
-      fuse_forces_for_tag<detail::GenericForceTag>(std::forward<Forces>(forces)...),
-      fuse_forces_for_tag<detail::PairwiseForceTag>(std::forward<Forces>(forces)...)
+      fuse_forces_for_tag<detail::GenericForceTag>(std::forward<Forces>(forces)...)
+      // fuse_forces_for_tag<detail::PairwiseForceTag>(std::forward<Forces>(forces)...)
     );
   }
 } // namespace kocs::detail
