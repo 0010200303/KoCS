66df8ec4a60c59f5c7690db1b0b5d3fd76f5653a
0010200303 <jonas.schoenbrodt@gmail.com>
2026-04-30 06:57:09 +0200

    testing

--- include/integrators/heun.hpp
+++ include/integrators/heun.hpp
@@ -12,18 +12,80 @@ namespace kocs::integrators {
   struct Heun : public Base<PairFinder, 4, Views...> {
     using Base<PairFinder, 4, Views...>::Base;
 
+  private:
+    template<typename FirstView, typename... RestViews>
+    KOKKOS_INLINE_FUNCTION
+    static void reset_pack_at(const detail::ViewPack<FirstView, RestViews...>& pack, const unsigned int i) {
+      pack.first()(i) = std::remove_cv_t<std::remove_reference_t<decltype(pack.first()(i))>>{};
+      reset_pack_at(static_cast<const typename detail::ViewPack<FirstView, RestViews...>::base_type&>(pack), i);
+    }
+
+    KOKKOS_INLINE_FUNCTION
+    static void reset_pack_at(const detail::ViewPack<>&, const unsigned int) { }
+
+    template<typename FirstView, typename... RestViews>
+    KOKKOS_INLINE_FUNCTION
+    static void predict_pack_at(
+      const detail::ViewPack<FirstView, RestViews...>& dst,
+      const detail::ViewPack<FirstView, RestViews...>& current,
+      const detail::ViewPack<FirstView, RestViews...>& delta,
+      const unsigned int i,
+      const double dt
+    ) {
+      dst.first()(i) = current.first()(i) + delta.first()(i) * dt;
+      predict_pack_at(
+        static_cast<const typename detail::ViewPack<FirstView, RestViews...>::base_type&>(dst),
+        static_cast<const typename detail::ViewPack<FirstView, RestViews...>::base_type&>(current),
+        static_cast<const typename detail::ViewPack<FirstView, RestViews...>::base_type&>(delta),
+        i,
+        dt
+      );
+    }
+
+    KOKKOS_INLINE_FUNCTION
+    static void predict_pack_at(
+      const detail::ViewPack<>&,
+      const detail::ViewPack<>&,
+      const detail::ViewPack<>&,
+      const unsigned int,
+      const double
+    ) { }
+
+    template<typename FirstView, typename... RestViews>
+    KOKKOS_INLINE_FUNCTION
+    static void correct_pack_at(
+      const detail::ViewPack<FirstView, RestViews...>& current,
+      const detail::ViewPack<FirstView, RestViews...>& delta0,
+      const detail::ViewPack<FirstView, RestViews...>& delta1,
+      const unsigned int i,
+      const double dt
+    ) {
+      current.first()(i) += (delta0.first()(i) + delta1.first()(i)) * 0.5 * dt;
+      correct_pack_at(
+        static_cast<const typename detail::ViewPack<FirstView, RestViews...>::base_type&>(current),
+        static_cast<const typename detail::ViewPack<FirstView, RestViews...>::base_type&>(delta0),
+        static_cast<const typename detail::ViewPack<FirstView, RestViews...>::base_type&>(delta1),
+        i,
+        dt
+      );
+    }
+
+    KOKKOS_INLINE_FUNCTION
+    static void correct_pack_at(
+      const detail::ViewPack<>&,
+      const detail::ViewPack<>&,
+      const detail::ViewPack<>&,
+      const unsigned int,
+      const double
+    ) { }
+
+  public:
     void apply_euler_predictor(float dt) {
       Kokkos::parallel_for(
         "apply_euler_predictor",
         this->agent_count,
         KOKKOS_CLASS_LAMBDA(const unsigned int i) {
-          this->stage_pack[2].apply([&](auto&... predicted_views) {
-            this->stage_pack[1].apply([&](auto&... delta_views) {
-              this->stage_pack[0].apply([&](auto&... current_views) {
-                ((predicted_views(i) += current_views(i) + delta_views(i) * dt), ...);
-              });
-            });
-          });
+          predict_pack_at(this->stage_pack[2], this->stage_pack[0], this->stage_pack[1], i, dt);
         }
       );
     }
@@ -33,24 +95,14 @@ namespace kocs::integrators {
         "apply_heun_corrector",
         this->agent_count,
         KOKKOS_CLASS_LAMBDA(const unsigned int i) {
-          // write old velocities
-          this->old_velocities(i) = (this->stage_pack[1].first()(i) + this->stage_pack[3].first()(i)) * 0.5;
+          this->old_velocities(i) =
+            (this->stage_pack[1].first()(i) + this->stage_pack[3].first()(i)) * 0.5;
 
-          this->stage_pack[0].apply([&](auto&... current_views) {
-            this->stage_pack[1].apply([&](auto&... delta_views_0) {
-              this->stage_pack[2].apply([&](auto&... predicted_views) {
-                this->stage_pack[3].apply([&](auto&... delta_views_1) {
-                  ((current_views(i) += (delta_views_0(i) + delta_views_1(i)) * 0.5 * dt), ...);
+          correct_pack_at(this->stage_pack[0], this->stage_pack[1], this->stage_pack[3], i, dt);
 
-                  ((delta_views_1(i) = std::remove_cv_t<std::remove_reference_t<decltype(delta_views_1(i))>>{}), ...);
-                });
-
-                ((predicted_views(i) = std::remove_cv_t<std::remove_reference_t<decltype(predicted_views(i))>>{}), ...);
-              });
-
-              ((delta_views_0(i) = std::remove_cv_t<std::remove_reference_t<decltype(delta_views_0(i))>>{}), ...);
-            });
-          });
+          reset_pack_at(this->stage_pack[1], i);
+          reset_pack_at(this->stage_pack[2], i);
+          reset_pack_at(this->stage_pack[3], i);
         }
       );
     }
