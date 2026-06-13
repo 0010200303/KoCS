d16c2f498097df6644b9f10685f319165240f649
0010200303 <jonas.schoenbrodt@gmail.com>
2026-06-13 15:16:20 +0200

    Testing DualView wrapper

--- include/types/view.hpp
+++ include/types/view.hpp
@@ -1,58 +1,44 @@
 #ifndef KOCS_TYPES_VIEW_HPP
 #define KOCS_TYPES_VIEW_HPP
 
-#include <cstdint>
 #include <string>
 
 #include <Kokkos_Core.hpp>
+#include <Kokkos_DualView.hpp>
 
 namespace kocs {
   template<typename T>
-  struct View {
-    public:
-      View(const std::string& label, const unsigned int count)
-        : device_view(label, count) { }
+  struct View : public Kokkos::DualView<T*> {
+    View(const std::string& label, const unsigned int count) : Kokkos::DualView<T*>(label, count) { }
 
-    private:
-      Kokkos::View<T*> device_view;
-      Kokkos::View<T*>::host_mirror_type host_view;
+    KOKKOS_INLINE_FUNCTION
+    T& operator()(int i) const {
+      KOKKOS_IF_ON_DEVICE((
+        return this->view_device()(i);
+      ))
 
-    public:
-      KOKKOS_INLINE_FUNCTION
-      T& operator()(int i) const {
-        KOKKOS_IF_ON_DEVICE((
-          return device_view(i);
-        ))
+      KOKKOS_IF_ON_HOST((
+        return this->view_host()(i);
+      ))
+    }
 
-        KOKKOS_IF_ON_HOST((
-          return host_view(i);
-        ))
-      }
+    // hide base class methods
+    inline void resize(const int value) {
+      Kokkos::DualView<T*>::resize(value);
+      this->sync_host();
+    }
 
-      inline void sync_host_to_device() {
-        Kokkos::deep_copy(device_view, host_view);
-      }
+    // unconditional sync
+    inline void sync_host_to_device() {
+      this->modify_host();
+      this->sync_device();
+    }
 
-      inline void sync_device_to_host() {
-        if (host_view.extent(0) != device_view.extent(0))
-          host_view = Kokkos::create_mirror_view(device_view);
-
-        Kokkos::deep_copy(host_view, device_view);
-      }
-
-      KOKKOS_INLINE_FUNCTION
-      unsigned int get_size() const {
-        return device_view.extent(0);
-      }
-
-      inline void resize(const unsigned int value) {
-        if (device_view.extent(0) == value)
-          return;
-
-        Kokkos::resize(device_view, value);
-
-        sync_device_to_host();
-      }
+    // unconditional sync
+    inline void sync_device_to_host() {
+      this->modify_device();
+      this->sync_host();
+    }
   };
 } // namespace kocs
 
--- main.cpp
+++ main.cpp
@@ -21,50 +21,51 @@ int main() {
   View<int> view("view", sim.get_capacity());
   sim.init_line();
 
-  auto generic = MAKE_GENERIC_FORCE_NAMED({
+  auto generic = GENERIC_FORCE(
     view(i) = i;
-    f.position.delta += Vector(0.0, 1.0, 0.0);
-    f.polarity.delta += Polarity(1.0);
-  });
+    ctx.position.delta += Vector(0.0, 1.0, 0.0);
+    ctx.polarity.delta += Polarity(1.0);
+  );
 
   sim.write();
   for (int i = 0; i < 4; ++i) {
     sim.take_step(dt, generic());
     sim.write();
   }
-
   view.sync_device_to_host();
 
-  Kokkos::parallel_for(view.get_size(), KOKKOS_LAMBDA(const unsigned int i) {
+  Kokkos::parallel_for(sim.get_agent_count(), KOKKOS_LAMBDA(const unsigned int i) {
     Kokkos::printf("%d\n", view(i));
   });
   Kokkos::fence();
 
   Kokkos::printf("\n");
 
-  for (int i = 0; i < view.get_size(); ++i)
+  for (int i = 0; i < sim.get_agent_count(); ++i)
     Kokkos::printf("%d\n", view(i));
 
   Kokkos::printf("=================================================\n");
-  unsigned int prev_size = view.get_size();
-  Kokkos::printf("%d\n", view.get_size());
+  unsigned int prev_size = sim.get_capacity();
+  Kokkos::printf("%d\n", sim.get_capacity());
+  sim.set_capacity(8);
+  sim.set_agent_count(8);
   view.resize(8);
-  Kokkos::printf("%d\n", view.get_size());
+  Kokkos::printf("%d\n", sim.get_capacity());
   Kokkos::printf("=================================================\n");
 
-  for (int i = prev_size; i < view.get_size(); ++i)
+  for (int i = prev_size; i < sim.get_agent_count(); ++i)
     view(i) = 10 + i;
   view.sync_host_to_device();
   Kokkos::fence();
 
-  Kokkos::parallel_for(view.get_size(), KOKKOS_LAMBDA(const unsigned int i) {
+  Kokkos::parallel_for(sim.get_agent_count(), KOKKOS_LAMBDA(const unsigned int i) {
     Kokkos::printf("%d\n", view(i));
   });
   Kokkos::fence();
 
   Kokkos::printf("\n");
 
-  for (int i = 0; i < view.get_size(); ++i)
+  for (int i = 0; i < sim.get_agent_count(); ++i)
     Kokkos::printf("%d\n", view(i));
 
   return 0;
